d7d23900946d218fea30f989285064a50c1f696f
weicracker <weicracker@163.com>
2019-09-05 20:51:26 +0800

    更新关于

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,6 +20,7 @@ class News extends React.Component {
                 <p>&nbsp; &nbsp; 一名90后Nodejs全栈程序员，常写Nodejs，Typescript，React，Python，Electron，偶尔写点Golang底层算法啥的，现任职某央企，业余爱好写些小程序、爬虫。</p>
                 <p>&nbsp; &nbsp; 喜欢逛一些博客论坛、钻研使用前沿的技术，喜欢科学上网，去外面的世界了解更前沿的技术；具有独立学习能力，责任心强、思维敏捷，能独立承担工作任务，具备良好的沟通能力。</p>
                 <p>&nbsp; &nbsp; 擅长钻研，对前端流行技术有浓厚兴趣，重视用户体验和网站交互功能的实现，并且付诸实际行动。</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;有严重的代码规范强迫症，封装强迫症，喜欢给朋友开发小爬虫、小程序。<br /></p>
                 <p><span style={{ fontWeight: "bold" }}>博客驱动：</span></p>
                 <p>&nbsp; &nbsp; 后端：Nodejs，服务使用Koa2框架，喜欢清爽简洁的代码，喜欢乔布斯的极简理念。</p>
                 <p>&nbsp; &nbsp; 前端：Gatsbyjs+React，博客采用简约配色，大道至简。<br /></p>
@@ -27,7 +28,6 @@ class News extends React.Component {
                 <p><span style={{ fontWeight: "bold" }}>联系方式：</span></p>
                 <p>&nbsp; &nbsp; 微信：wogeinishu</p>
                 <p>&nbsp; &nbsp; 邮箱：jiwei3778@gmail.com</p>
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;有严重的代码规范强迫症，封装强迫症，喜欢给朋友开发小爬虫、小程序。<br /></p>
               </div>
             </div>
           </article>
